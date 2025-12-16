ad9818b618df2031e9b4e719df03672fb0597a0f
GaetanMeyer <gaetanmeyer185@gmail.com>
2025-12-17 00:36:40 +0100

    move and partialJump

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,9 +5,13 @@ public class Player : MonoBehaviour
 {
     
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
 
+
+    private float _moveInput;
+    private float _jumpInput;
     
-    private Vector2 player;
+    //private Vector2 player;
     private Rigidbody2D _prb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,17 +22,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float HorizontalInput = Input.GetAxis("Horizontal");
+        //float HorizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(_moveInput * _speed, Time.deltaTime);
+        _prb.linearVelocity = movement;
         
-        _prb.linearVelocity =  Vector2.right * HorizontalInput * _speed;
         
-        
+        _prb.AddForce(transform.up * _jumpForce * _jumpInput);
+
+
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
-        Debug.Log("is walking" + ctx.ReadValue<float>());
-        _prb.linearVelocity = Vector2.right * ctx.ReadValue<float>();
+        Debug.Log("is walking : " + ctx.ReadValue<float>());
+        _moveInput = ctx.ReadValue<float>();
+        
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("is jumping : " + ctx.ReadValue<float>());
+        _jumpInput = ctx.ReadValue<float>();
         
     }
 }
