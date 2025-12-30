23d7a9400bf775d8cb7b6e76a8c0b7649472cae6
GaetanMeyer <gaetanmeyer185@gmail.com>
2025-12-30 15:19:22 +0100

    jump using boxCast

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -10,7 +10,12 @@ public class PlayerMove : MonoBehaviour
     
     private float _moveInput;
     private float _jumpInput;
-    private bool _isGrounded = false;
+
+    [SerializeField] private Vector2 _boxSize;
+    
+    [SerializeField ]private float _castDistance;
+    [SerializeField]private LayerMask _groundLayer;
+    //private bool _isGrounded = false;
     
     
     private Rigidbody2D _prb;
@@ -31,10 +36,14 @@ public class PlayerMove : MonoBehaviour
         Vector2 movement = new Vector2(_moveInput * _speed, Time.fixedDeltaTime);
         _prb.linearVelocity = movement;
 
-        if (_isGrounded == true)
+        if (IsGrounded())
         {
             _prb.AddForce(Vector2.up * _jumpInput * _jumpForce, ForceMode2D.Impulse);
         }
+        // if (_isGrounded == true)
+        // {
+        //     _prb.AddForce(Vector2.up * _jumpInput * _jumpForce, ForceMode2D.Impulse);
+        // }
 
     }
 
@@ -53,27 +62,45 @@ public class PlayerMove : MonoBehaviour
         
      }
 
-    // public void OnDash(InputAction.CallbackContext ctx)
+     public bool IsGrounded()
+     {
+         if (Physics2D.BoxCast(transform.position, _boxSize, 0, -transform.up, _castDistance, _groundLayer ))
+         {
+             return true;
+         }
+         else
+         {
+             return false;
+         }
+     }
+
+     private void OnDrawGizmos()
+     {
+         Gizmos.DrawWireCube(transform.position-transform.up * _castDistance, _boxSize);
+     }
+
+
+     // public void OnDash(InputAction.CallbackContext ctx)
     // {
     //     Debug.Log("is dashing : " + ctx.ReadValue<float>());
     //     _dashInput = ctx.ReadValue<float>();
     // }
 
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            Debug.Log("is grounded");
-            _isGrounded = true;
-        }
-    }
-
-    public void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            Debug.Log("air");
-            _isGrounded = false;
-        }
-    }
+    // public void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Ground"))
+    //     {
+    //         Debug.Log("is grounded");
+    //         _isGrounded = true;
+    //     }
+    // }
+    //
+    // public void OnCollisionExit2D(Collision2D other)
+    // {
+    //     if (other.gameObject.CompareTag("Ground"))
+    //     {
+    //         Debug.Log("air");
+    //         _isGrounded = false;
+    //     }
+    // }
 }
